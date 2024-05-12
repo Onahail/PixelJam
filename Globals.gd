de86de67f7bbb1be6aacb42519ac5fef0d3faffc
Onahail <mahavemann@gmail.com>
2024-05-11 15:36:18 -1000

    Added Global variables

---
+++ Globals.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var testvar = 1000
--- Menus/mission_select.gd
+++ Menus/mission_select.gd
@@ -1,7 +1,13 @@
 extends Node2D
 
+func _ready():
+	var globals = get_node("res://Globals.gd")
+
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://Menus/main_menu.tscn")
 
 func _on_mission_start_pressed():
 	get_tree().change_scene_to_file("res://Game/game.tscn")
+
+func _on_depth_selection_pressed():
+	print(Globals.testvar)
